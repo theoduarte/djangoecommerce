5617b0bebe3c09b3df44a8e4587353036012216a
theogenes <teocomp@gmail.com>
2017-03-01 18:55:29 -0300

    aula 26-31

--- catalog/forms.py
+++
@@ -1,7 +0,0 @@
-# coding=utf-8
-
-from django import forms
-
-class ContactForm(forms.Form):
-    
-    name = forms.CharField(label='Nome')
--- core/views.py
+++ core/views.py
@@ -2,8 +2,12 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.conf import settings
 
-from catalog.models import Category
+#from catalog.models import Category
+from .forms import ContactForm
+import pprint
 
 # Create your views here.
 
@@ -12,14 +16,34 @@ def index(request):
     texts = ['desenvolvendo com Django Python', ' com curso Udemy']
     context = {
         'title': 'django E-Commerce',
-        'texts': texts,
-        'categories': Category.objects.all()
+        'texts': texts
+        #'categories': Category.objects.all()
     }
     
     return render(request, 'index.html', context)
 
 def contact(request):
-    return render(request, 'contact.html')
+    
+    form = ContactForm(request.POST or None)
+
+    success = False
+
+    if form.is_valid():
+        form.send_mail()
+        success = True
+    
+    context = {
+        'form': form,
+        'success': success
+    }
+    
+#    pprint.isrecursive(form)
+#    pprint.pprint(form, depth=4)
+#    pprint.pprint(form.as_p)
+#    {{ form.as_p }}
+    
+    
+    return render(request, 'contact.html', context)
 
 def product(request):
     return render(request, 'product.html')
