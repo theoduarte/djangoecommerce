9eb6403f6f6fcfe67a6691f3d85d8db54a09b37e
theogenes <teocomp@gmail.com>
2017-03-02 18:34:20 -0300

    commit accounts home user page2 built in

---
+++ accounts/views.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+from django.shortcuts import render
+from django.views.generic import CreateView, TemplateView
+from django.core.urlresolvers import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .models import User
+from .forms import UserAdminCreationForm
+
+
+class IndexView(LoginRequiredMixin, TemplateView):
+
+    template_name = 'accounts/index.html'
+
+
+class RegisterView(CreateView):
+
+    model = User
+    template_name = 'accounts/register.html'
+    form_class = UserAdminCreationForm
+    success_url = reverse_lazy('index')
+
+
+index = IndexView.as_view()
+register = RegisterView.as_view()
