294059500e5a77c22027e16d55b89843cd9293cc
theogenes <theo.duarte@icloud.com>
2017-03-03 11:00:07 -0300

    carrinho de compras

--- core/views.py
+++ core/views.py
@@ -8,6 +8,7 @@ from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import View, TemplateView, CreateView
 from django.contrib.auth import get_user_model
+from django.contrib import messages
 
 #from catalog.models import Category
 from .forms import ContactForm
@@ -64,6 +65,8 @@ def contact(request):
     if form.is_valid():
         form.send_mail()
         success = True
+    elif request.method == 'POST':
+        messages.error(request, 'Formulário inválido')
 
     context = {
         'form': form,
